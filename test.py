a903b83b8c25a9e22336e034e3dc704a736ac31d
yanqiang-bpa <yanqiang051@sohu.com>
2015-10-23 14:18:32 +0800

    Add test code about re module of python

--- test.py
+++ test.py
@@ -1,18 +1,22 @@
-dic = {
-	"status" : 2,
-	"samples" : [ 
-			       {
-			            "library_plate_id" : 1,
-			            "library_plate": "abc"
-			       }
-			    ]
-}
+# dic = {
+# 	"status" : 2,
+# 	"samples" : [ 
+# 			       {
+# 			            "library_plate_id" : 1,
+# 			            "library_plate": "abc"
+# 			       }
+# 			    ]
+# }
 
-sample = {
-			            "library_plate_id" : 2,
-			            "library_plate": "dkfjd"
-			       }
+# sample = {
+# 			            "library_plate_id" : 2,
+# 			            "library_plate": "dkfjd"
+# 			       }
 
-dic["samples"].append(sample)
+# dic["samples"].append(sample)
 
-print dic
+# print dic
+
+
+import re
+print(re.match( r'\d{8}', '20150623')).group(0)
